132d46063c34d95fc5accf0d781b75a74a7792a2
KimChangmin <zzangmin3@gmail.com>
2024-03-31 19:29:43 +0900

    error

---
+++ next-project/src/pages/_app.js
@@ -0,0 +1,14 @@
+import "../components/sharing/styles/reset.css";
+import { SharedPage } from "../components/SharedPage";
+import { FolderPage } from "../components/FolderPage";
+
+function App() {
+  return (
+    <>
+      <SharedPage />
+      <FolderPage />
+    </>
+  );
+}
+
+export default App;
---
+++ next-project/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./_app";
+
+export default function Index() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
