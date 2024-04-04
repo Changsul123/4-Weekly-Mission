65e6fa00467b0e7be7e24cc3ad0f73b4f4d1e06c
KimChangmin <zzangmin3@gmail.com>
2024-04-04 21:16:49 +0900

    refactor:migrate input.jsx to tsx

--- next-project-directory/src/component/input.tsx
+++ next-project-directory/src/component/input.tsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import styles from "./input.module.scss";
 
+interface Props {
+  placeholder: string;
+  disabled: boolean;
+  error: { message: string };
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 export default function TextFieldInput({
   placeholder,
   disabled,
   error,
   handleChange,
   ...props
-}) {
+}: Props) {
   const [status, setStatus] = useState("inActive");
 
   const handleFocus = () => {
     setStatus("focused");
   };
 
-  const handleBlur = (e) => {
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
     if (!e.target.value) {
       error.message = "값을 입력해 주세요.";
       setStatus("error");
@@ -23,7 +29,7 @@ export default function TextFieldInput({
     }
   };
 
-  const handleInputChange = (e) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value) {
       setStatus("active");
     }
@@ -45,7 +51,6 @@ export default function TextFieldInput({
         className={`${styles.StyledInput} ${styles[status]}`}
         placeholder={placeholder}
         type="text"
-        $status={status}
         onFocus={handleFocus}
         onBlur={handleBlur}
         onChange={handleInputChange}
